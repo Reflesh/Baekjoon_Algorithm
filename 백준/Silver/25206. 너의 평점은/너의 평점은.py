281b5527ca002a58c71f0f17a66e113d4107c810
Reflesh <160117532+Reflesh@users.noreply.github.com>
2024-09-29 17:18:58 +0900

    [Silver V] Title: 너의 평점은, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.py
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -1,40 +1,13 @@
+grade = ['A+', 'A0', 'B+', 'B0', 'C+', 'C0', 'D+', 'D0', 'F']
+credit = [4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0]
 sum_credit = 0
 sum_grade = 0
 
-for i in range(20):
-    name, credit, grade = input().split()
-    float_cd = float(credit)
-    if grade == 'A+':
-        sum_grade += float_cd * 4.5 
-        sum_credit += float_cd
-    elif grade == 'A0':
-        sum_grade += float_cd * 4.0
-        sum_credit += float_cd
-    elif grade == 'B+':
-        sum_grade += float_cd * 3.5
-        sum_credit += float_cd
-    elif grade == 'B0':
-        sum_grade += float_cd * 3.0
-        sum_credit += float_cd
-    elif grade == 'C+':
-        sum_grade += float_cd * 2.5
-        sum_credit += float_cd
-    elif grade == 'C0':
-        sum_grade += float_cd * 2.0
-        sum_credit += float_cd
-    elif grade == 'D+':
-        sum_grade += float_cd * 1.5
-        sum_credit += float_cd
-    elif grade == 'D0':
-        sum_grade += float_cd * 1.0
-        sum_credit += float_cd
-    elif grade == 'F':
-        sum_credit += float_cd
-    else:
-        continue
-
-if sum_credit != 0:
-    avg = sum_grade / sum_credit
-    print(avg)
-else:
-    print(0)
+for _ in range(20):
+    name, c, g = input().split()
+    c = float(c)
+    if g != 'P':
+        sum_credit += c
+        sum_grade += c * credit[grade.index(g)]
+        
+print(sum_grade / sum_credit)
