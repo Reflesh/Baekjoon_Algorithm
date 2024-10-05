b7d3f9a7c484103d24e1866f8e967b0a8c7018cd
Reflesh <160117532+Reflesh@users.noreply.github.com>
2024-10-05 11:08:30 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,17 +1,8 @@
 t = int(input())
-
-for i in range(t):
+  
+for _ in range(t):
     cent = int(input())
-    quarter = cent // 25
-    cent -= quarter * 25
-    
-    dime = cent // 10
-    cent -= dime * 10
-    
-    nickel = cent // 5
-    cent -= nickel * 5
-    
-    penny = cent
-    
-    print(quarter, dime, nickel, penny, end = " ")
-    print()
+    for i in [25, 10, 5, 1]:
+        print(cent // i, end = ' ')
+        cent = cent % i
+    print()  # 가독성을 위해 작성
