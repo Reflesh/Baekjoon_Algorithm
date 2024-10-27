7a15e8285e08e52055f55eea3015fdbdab4009d7
Reflesh <160117532+Reflesh@users.noreply.github.com>
2024-10-27 14:02:48 +0900

    Update 수학은 비대면강의입니다.py

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -1,3 +1,4 @@
+### brute force version
 a, b, c, d, e, f = map(int, input().split())
 
 for x in range(-999, 1000):
@@ -5,3 +6,24 @@ for x in range(-999, 1000):
         if a * x + b * y == c and d * x + e * y == f:
             print(x, y)
             break
+
+
+### z3 module version
+from z3 import *
+
+a, b, c, d, e, f = map(int, input().split())
+
+x = Int('x')
+y = Int('y')
+solver = Solver()
+
+solver.add(a * x + b * y == c)
+solver.add(d * x + e * y == f)
+solver.add(And(x >= -999, x <= 999))
+solver.add(And(y >= -999, y <= 999))
+
+if solver.check() == sat:
+    solution = solver.model()
+    print(solution[x], solution[y])
+else:
+    print("No solution")
