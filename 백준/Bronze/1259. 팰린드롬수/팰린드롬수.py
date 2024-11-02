b985d543e4b6076e92654084d59229a4042ac786
Reflesh <160117532+Reflesh@users.noreply.github.com>
2024-11-02 21:49:49 +0900

    [Bronze I] Title: 팰린드롬수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,6 +1,6 @@
 while True:
     Palindrome = list(map(int, input().strip()))
-    if Palindrome[0] == 0 and len(Palindrome) == 1:
+    if Palindrome[0] == 0:
         break
     if Palindrome[0:len(Palindrome)] == Palindrome[::-1]:
         print('yes')
