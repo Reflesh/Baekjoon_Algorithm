69127dd72677aaef2edfeb34ec82250a09e9a941
Reflesh <160117532+Reflesh@users.noreply.github.com>
2024-11-02 19:05:42 +0900

    Update 좌표 정렬하기.py

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -9,3 +9,15 @@ loc_list_sorted = sorted(loc_list)
 
 for x, y in loc_list_sorted:
     print(x, y)
+
+#n = int(input())
+#loc_list = []
+
+#for i in range(n):
+#    x, y = map(int, input().split())
+#    loc_list.append((x, y))
+
+#loc_list.sort(key = lambda x: (x[0], x[1]))
+
+#for x, y in loc_list:
+#    print(x, y)
