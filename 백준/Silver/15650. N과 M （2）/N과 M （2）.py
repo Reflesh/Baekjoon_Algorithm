eb420edca91452d34c5ee9bd0519f5dc58f38973
Reflesh <160117532+Reflesh@users.noreply.github.com>
2024-12-24 14:06:01 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,19 @@
+def generate_sequences(num, length):
+    result = []
+    def backtrack(curr_seq, start):
+        if len(curr_seq) == length:
+            result.append(curr_seq.copy())
+            return
+        for i in range(start, num+1):
+            if i not in curr_seq:
+                curr_seq.append(i)
+                backtrack(curr_seq, i+1)
+                curr_seq.pop()
+
+    backtrack([], 1)
+    return result
+
+num, length = map(int, input().split())
+sequences = generate_sequences(num, length)
+for res in sequences:
+    print(*res)
