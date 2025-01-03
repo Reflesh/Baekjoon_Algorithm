1bc3b6783119584061776f231faf2fdb6a6f8852
Reflesh <160117532+Reflesh@users.noreply.github.com>
2025-01-03 15:22:08 +0900

    [Bronze III] Title: 오븐 시계, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -1,16 +1,11 @@
-h, m  = map(int, input().split())
-c = int(input())
-add_hour = 0
-if(m+c>=60):
-    add_hour=(m+c)//60
-    real_min = m+c-add_hour*60
-else :
-    real_min = m+c
-
-
-if(h+add_hour>23):
-    real_hour = h+add_hour-24
-else : 
-    real_hour = h+add_hour 
-
-print(f"{int(real_hour)} {int(real_min)}")
+h, m = map(int, input().split())
+t = int(input())
+m += t
+while m >= 60:
+    m -= 60
+    h += 1
+if h >= 24:
+    h -= 24
+    print(h, m)
+else:
+    print(h, m)
