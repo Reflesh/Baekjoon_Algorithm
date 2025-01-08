c0129236102b95e4b03a5eda8bf45705a764912d
Reflesh <160117532+Reflesh@users.noreply.github.com>
2025-01-08 14:53:42 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -0,0 +1,18 @@
+num = int(input())
+
+def sol(n):
+    result = [[0] * 10 for _ in range(n + 1)]
+    
+    for i in range(1, 10):
+        result[1][i] = 1
+        
+    for i in range(2, n + 1):
+        for j in range(10):
+            if j > 0:
+                result[i][j] = result[i - 1][j - 1]
+            if j < 9:
+                result[i][j] += result[i - 1][j + 1]
+        
+    return sum(result[n])
+
+print(sol(num) % 1000000000)
