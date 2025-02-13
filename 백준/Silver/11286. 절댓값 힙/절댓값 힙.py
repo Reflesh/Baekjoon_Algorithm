98ca49fd734d665bbc75f14802629493cd12cb6f
Reflesh <160117532+Reflesh@users.noreply.github.com>
2025-02-13 10:26:53 +0900

    [Silver I] Title: 절댓값 힙, Time: 104 ms, Memory: 36776 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -0,0 +1,26 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+cnt = int(input())
+max_heap = []
+min_heap = []
+for _ in range(cnt):
+    x = int(input())
+    if x == 0: # 결과값 출력
+        if min_heap:
+            if max_heap: # 최소힙과 최대힙이 둘 다 존재할 때
+                if min_heap[0] < max_heap[0]:
+                    print(heapq.heappop(min_heap))
+                else:
+                    print(-heapq.heappop(max_heap))
+            else: # 최소힙만 존재할 때 
+                print(heapq.heappop(min_heap))
+        elif max_heap:
+            print(-heapq.heappop(max_heap))
+        else:
+            print(0)
+    elif x > 0: # 음수일때
+        heapq.heappush(min_heap, x)
+    elif x < 0: # 양수일때
+        heapq.heappush(max_heap, -x)
