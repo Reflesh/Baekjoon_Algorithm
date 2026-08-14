52dcf0d0ab51b82b491707506cd54b203117cc1a
Reflesh <160117532+Reflesh@users.noreply.github.com>
2026-08-14 17:17:09 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 2.48 ms, Memory: 4.76 MB -BaekjoonHub

---
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+    
+    for(char c : str) {
+        if(isupper(c)) {
+            cout << (char)tolower(c);
+        } else {
+            cout << (char)toupper(c);
+        }
+    }
+    return 0;
+}
